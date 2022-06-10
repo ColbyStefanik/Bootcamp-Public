e89984db52cdd41cb7a2d3b1813f3d4c33f24d55
Colby Stefanik <colbster190@gmail.com>
2022-06-10 12:28:30 -0700

    MERN commit for 6/10

---
+++ MERN_Stack/Week_1/Stefanik_Colby_Predict_Destructuring.js
@@ -0,0 +1,60 @@
+//Code snippets from the Coding Dojo, assignment is to predict the outcome of each snippet.
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//randomCar would be Tesla, while otherRandomCar would be Mercedes
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+//name would provide a null value, while otherName would be Elon
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//password would give '12345' while hashedPassword would be a null value
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+//first == second would be '2 == 5' while first == third would be '2 == 2'
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+//key would give 'value' while secondKey would give '1, 5, 1, 8, 3, 3' while secondKey[0] would give '1' while willThisWork would give '5'
